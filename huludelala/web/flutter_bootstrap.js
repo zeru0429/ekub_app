62ab46c82d6e92a56e8666be66a4be3fdbd3d1a5
zeru0429 <mihiretut21@gmail.com>
2024-08-07 09:58:37 -0400

    some pages ui added 3

--- huludelala/web/flutter_bootstrap.js
+++ huludelala/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "854247476"
+    serviceWorkerVersion: "1482718091"
   }
 });
